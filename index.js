ba3f1be7e05d27bf12853a5f90e1fd829e13e2e2
unknown <shefata69@gmail.com>
2023-06-12 03:58:54 +0600

    user role

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -29,9 +30,19 @@ async function run() {
     const usersCollection = client.db("SummerSchool").collection("users");
     const classesCollection = client.db("SummerSchool").collection('classes');
     const instructorCollection = client.db("SummerSchool").collection('instructor');
-    const selectedClassCollection = client.db("SummerSchool").collection("selectedClass")
+    const selectedClassCollection = client.db("SummerSchool").collection("selectedClass");
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
 
     // users
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
@@ -44,6 +55,34 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'Admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'Instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
     // classes
     app.get('/classes', async (req, res) => {
